20f7b7c724d5f1468bab1999e5a99894a972d0db
Kaitlyn <kaitlyn.a.goodman@gmail.com>
2021-09-03 03:35:41 +0000

    finished third section

--- objects.js
+++ objects.js
@@ -4,10 +4,30 @@
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+  const words = phrase.split(" ");
+  const wordCount = {};
+  for (const word of words) {
+    if (!wordCount[word]) {
+      wordCount[word] = 1;}
+     else {
+        wordCount[word] += 1;}
+  }
+   return wordCount;     
+  
 }
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
   // Replace this with your code
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+  if (!melonPrices[price]) {
+    return;
+  }
+  return melonPrices[price].sort();
 }
