13f19b28dd3f89bc874fb16ca138bd624af6c4e2
Kaitlyn <kaitlyn.a.goodman@gmail.com>
2021-09-03 03:22:42 +0000

    completed first two sections

--- arrays.js
+++ arrays.js
@@ -4,16 +4,30 @@
 // 1. printIndices
 function printIndices(items) {
   // Replace this with your code
+  for (const i in items) {
+    console.log(`${items[i]} ${i}`);
+}
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+  const result = [];
+
+  for (const i in items) {
+    if (i % 2 === 0) {
+      result.push(items[i]);
+    }
+  }
+  console.log(result);
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
   // Replace this with your code
-}
+  const itemsSorted = items.sort((a, b) => a - b);
+  const sliced = items.slice(0, n);
+  console.log(sliced.reverse());
+} 
--- functions.js
+++ functions.js
@@ -4,13 +4,39 @@
 // 1. isHometown
 
 // Define your function here
-
+function isHometown(town) {
+    return town === 'San Francisco';
+}
 
 // 2. getFullName
 
 // Define your function here
+// function getFullName(firstName, lastName) {
+//     return `${firstName} ${lastName}`;
+// }
+const getFullName = (firstName, lastName) => `${firstName} ${lastName}`
 
 
 // 3. calculateTotal
 
 // Define your function here
+
+const calculateTotal = (basePrice, state, tax = 0.05) => {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;
+    }
+    else if (state === 'PA') {
+        fee = 2;
+    }
+    else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        }
+        else {
+            fee = 3;
+        }
+    }
+    return subtotal + fee;
+}
